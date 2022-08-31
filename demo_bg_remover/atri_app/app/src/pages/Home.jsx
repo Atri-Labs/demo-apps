4639e65db93dc0a0da1a6468873f3d09ade3dce4
sanskargupta <sanskarg348@gmail.com>
2022-08-31 23:29:44 +0530

    added the background-remover/changer

---
+++ demo_bg_remover/atri_app/app/src/pages/Home.jsx
@@ -0,0 +1,82 @@
+import { useLayoutEffect, useEffect } from "react";
+import useStore, { setEffectivePropsForPage, updateStoreStateFromController } from "../hooks/useStore";
+import useIoStore from "../hooks/useIoStore";
+import { useNavigate, useLocation } from "react-router-dom";
+import { subscribeInternalNavigation } from "../utils/navigate";
+import {fetchPageProps} from "../utils/fetchPageProps"
+import { Flex } from "@atrilabs/react-component-manifests/src/manifests/Flex/Flex.tsx";
+import { TextBox } from "@atrilabs/react-component-manifests/src/manifests/TextBox/TextBox.tsx";
+import { Upload } from "@atrilabs/react-component-manifests/src/manifests/Upload/Upload.tsx";
+import { Image } from "@atrilabs/react-component-manifests/src/manifests/Image/Image.tsx";
+import { Button } from "@atrilabs/react-component-manifests/src/manifests/Button/Button.tsx";
+import { useFlex1Cb, useTextBox1Cb, useFlex3Cb, useFlex5Cb, useUpload1Cb, useTextBox2Cb, useImage2Cb, useButton1Cb } from "../page-cbs/Home";
+
+export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsub = subscribeInternalNavigation((url) => {
+      navigate(url);
+    });
+    return unsub;
+  }, [navigate]);
+
+  const location = useLocation();
+  useLayoutEffect(()=>{
+    fetchPageProps(location.pathname, location.search).then((res)=>{
+      updateStoreStateFromController(res.pageName, res.pageState)
+    })
+  }, [])
+
+  useLayoutEffect(()=>{
+    setEffectivePropsForPage("Home")
+  }, [])
+
+  useEffect(()=>{
+    const onResize = ()=>{
+      setEffectivePropsForPage("Home")
+    };
+    window.addEventListener("resize", onResize);
+    return ()=>{
+      window.removeEventListener("resize", onResize);
+    }
+  }, [])
+
+  const Flex1Props = useStore((state)=>state["Home"]["Flex1"]);
+const Flex1IoProps = useIoStore((state)=>state["Home"]["Flex1"]);
+const Flex1Cb = useFlex1Cb()
+const TextBox1Props = useStore((state)=>state["Home"]["TextBox1"]);
+const TextBox1IoProps = useIoStore((state)=>state["Home"]["TextBox1"]);
+const TextBox1Cb = useTextBox1Cb()
+const Flex3Props = useStore((state)=>state["Home"]["Flex3"]);
+const Flex3IoProps = useIoStore((state)=>state["Home"]["Flex3"]);
+const Flex3Cb = useFlex3Cb()
+const Flex5Props = useStore((state)=>state["Home"]["Flex5"]);
+const Flex5IoProps = useIoStore((state)=>state["Home"]["Flex5"]);
+const Flex5Cb = useFlex5Cb()
+const Upload1Props = useStore((state)=>state["Home"]["Upload1"]);
+const Upload1IoProps = useIoStore((state)=>state["Home"]["Upload1"]);
+const Upload1Cb = useUpload1Cb()
+const TextBox2Props = useStore((state)=>state["Home"]["TextBox2"]);
+const TextBox2IoProps = useIoStore((state)=>state["Home"]["TextBox2"]);
+const TextBox2Cb = useTextBox2Cb()
+const Image2Props = useStore((state)=>state["Home"]["Image2"]);
+const Image2IoProps = useIoStore((state)=>state["Home"]["Image2"]);
+const Image2Cb = useImage2Cb()
+const Button1Props = useStore((state)=>state["Home"]["Button1"]);
+const Button1IoProps = useIoStore((state)=>state["Home"]["Button1"]);
+const Button1Cb = useButton1Cb()
+
+  return (<>
+  <Flex {...Flex1Props} {...Flex1Cb} {...Flex1IoProps}>
+<TextBox {...TextBox1Props} {...TextBox1Cb} {...TextBox1IoProps}/>
+</Flex>
+<Flex {...Flex3Props} {...Flex3Cb} {...Flex3IoProps}>
+<Image {...Image2Props} {...Image2Cb} {...Image2IoProps}/>
+<Flex {...Flex5Props} {...Flex5Cb} {...Flex5IoProps}>
+<Upload {...Upload1Props} {...Upload1Cb} {...Upload1IoProps}/>
+<TextBox {...TextBox2Props} {...TextBox2Cb} {...TextBox2IoProps}/>
+</Flex>
+<Button {...Button1Props} {...Button1Cb} {...Button1IoProps}/>
+</Flex>
+  </>);
+}
